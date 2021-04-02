37588d7a34fcdd478191cbd697c0731b357433c1
Dathan Vance Pattishall <dathanvp@gmail.com>
2021-04-02 06:46:17 -0700

    fix

--- async.go
+++ async.go
@@ -1,21 +1,22 @@
 package casync
 
 import (
-	"sync"
+	"fmt"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
-	"fmt"
 )
 
 type Async struct {
-	TasksChannel chan *Task // tasks to execute
-	Concurrency  int // how many go routines should be active.
+	TasksChannel chan *Task     // tasks to execute
+	Concurrency  int            // how many go routines should be active.
 	wg           sync.WaitGroup // control the go routines
-	Tasks        []*Task // list of tasks for this async struct
+	Tasks        []*Task        // list of tasks for this async struct
 	Sigs         chan os.Signal // catch unix sigs to graceful shutdown
-	stop         bool // tell the app to stop
+	stop         bool           // tell the app to stop
+	done         bool
 }
 
 //
@@ -25,10 +26,10 @@ func NewAsync(sizeofCurrencency int, ts []*Task) *Async {
 
 	as := &Async{
 		TasksChannel: make(chan *Task, len(ts)), // make a channel to a pointer of task
-		Concurrency: sizeofCurrencency,
-		Tasks: ts,
-		Sigs : make(chan os.Signal, 1),
-		stop : false,
+		Concurrency:  sizeofCurrencency,
+		Tasks:        ts,
+		Sigs:         make(chan os.Signal, 1),
+		stop:         false,
 	}
 
 	signal.Notify(as.Sigs, syscall.SIGINT, syscall.SIGTERM) // let as.sigs know if sigtem or sigint happened
@@ -39,7 +40,6 @@ func NewAsync(sizeofCurrencency int, ts []*Task) *Async {
 	return as
 
 }
-
 
 //
 // allways run size of concurrency Tasks. For instance if Async.Concurrency == 4, 4 gouroutines will dequeue Tasks to
@@ -55,35 +55,32 @@ func (a *Async) setupWorkers() {
 			defer a.wg.Done()
 
 			for task := range a.TasksChannel {
-				// read off all the tasks in each routine
-
-				if a.stop == false {
-					// do not execute any more tasks
-					if task.timeout_sec > 0 {
-
-						ch := make(chan bool, 1)
-						go func() {
-							task.Execute()
-							ch <- true
-						}()
-
-						select {
-						case <-ch:
-							fmt.Println("Finish: ", task.id)
-							continue
-						case <-time.After(task.timeout_sec):
-							fmt.Println("TIMEOUT: ", task.id)
-							continue
-						}
-
-					} else {
-						task.Execute()
-					}
-
-				} else {
+				// got a signal to stop do not execute anymore tasks
+				if a.stop == true {
 					return
 				}
-			}
+
+				if task.timeout_sec <= 0 {
+					task.Execute()
+					continue
+				} else {
+
+					ch := make(chan bool, 1)
+					go func() {
+						task.Execute()
+						ch <- true
+					}()
+
+					select {
+					case <-ch:
+						fmt.Println("Finish: ", task.id)
+						continue
+					case <-time.After(task.timeout_sec):
+						fmt.Println("TIMEOUT: ", task.id)
+						continue
+					}
+				}
+			} // TasksChannel
 		}()
 	}
 }
@@ -99,9 +96,16 @@ func (a *Async) ExecuteTasks() {
 
 	close(a.TasksChannel)
 	a.wg.Wait() // block until the N go routines stop
-
+	a.done = true
 }
 
+// helper to see if the jobs are done
+func (a *Async) IsDone() bool {
+	if a.done == true || a.stop == true {
+		return true
+	}
+	return false
+}
 
 //
 // catch a signal and drain the tasks
@@ -111,8 +115,8 @@ func (a *Async) WatchTasks() {
 	for {
 		select {
 		case <-a.Sigs:
-		// don't care what the signal is we will just stop the async process
-			a.stop = true;
+			// don't care what the signal is we will just stop the async process
+			a.stop = true
 			return // break out of the loop and Exit
 
 		}
